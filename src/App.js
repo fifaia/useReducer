071b96758afe3b2856daacad3be9f3a64ab5a663
fifaia <36516921+fifaia@users.noreply.github.com>
2022-03-11 00:50:23 +0400

    bla

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 import { reducer, initialState } from "./reducer";
 import { useReducer, useState } from "react";
-
+// bla ba
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
@@ -9,7 +9,7 @@ export default function App() {
     dispatch(e.target.value);
     console.log(e.target.value);
   };
-
+  //
   return (
     <div className="App">
       <h2>{state.number}</h2>
